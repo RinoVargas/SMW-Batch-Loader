fce13bb90472170a7739b4755242cdf9f3d30fde
Rino Varas Quintana <rino.vargas.quintana@gmail.com>
2021-09-30 14:20:59 -0500

    Create FACTUNIF Database Connector

---
+++ src/main/java/cl/entel/eai/config/FACTUNIFDatabaseConnector.java
@@ -0,0 +1,15 @@
+package cl.entel.eai.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("factunifConnector")
+public class FACTUNIFDatabaseConnector extends DatabaseConnector{
+
+    public FACTUNIFDatabaseConnector(
+            @Value("${factunif.db.url}")  String url,
+            @Value("${factunif.db.username}") String username,
+            @Value("${factunif.db.password}") String password) {
+        super(url, username, password);
+    }
+}
