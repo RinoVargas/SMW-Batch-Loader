026a0075309a0c65ac6a2fed2f586558fe42acec
Rino Varas Quintana <rino.vargas.quintana@gmail.com>
2021-09-30 14:08:01 -0500

    Add logging

--- src/main/java/cl/entel/eai/runner/DAOPipelineRunner.java
+++ src/main/java/cl/entel/eai/runner/DAOPipelineRunner.java
@@ -2,13 +2,19 @@ package cl.entel.eai.runner;
 
 import cl.entel.eai.exception.PipelineException;
 import cl.entel.eai.pipeline.configuration.DAOConfiguration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public abstract class DAOPipelineRunner<D, O> extends PipelineRunner<DAOConfiguration<D>, O>{
+
+    private static final Log logger = LogFactory.getLog(DAOPipelineRunner.class);
 
     DAOPipelineRunner() { }
 
     @Override
     public void run() throws PipelineException{
+
+        logger.info("Inicializando carga de registros...");
         this.init();
 
         long offset = this.getReader().getConfiguration().getOffset();
@@ -18,11 +24,18 @@ public abstract class DAOPipelineRunner<D, O> extends PipelineRunner<DAOConfigur
             this.getReader().getConfiguration().computeChuckSize();
             long chunkSize = this.getReader().getConfiguration().getChunkSize();
 
+
             this.executePipeline();
 
             offset += chunkSize;
             this.getReader().getConfiguration().incrementOffset(chunkSize);
+
+            logger.info(String.format("Lote de registros cargados: %d / %d", offset, total));
         }
+
+        logger.info("Finalizando carga de registros...");
+        this.onFinish();
+        logger.info(String.format("Total de Registros cargados: %d", total));
     }
 
     private long computeChuckSize(long offset, long chunkSize, long total) {
