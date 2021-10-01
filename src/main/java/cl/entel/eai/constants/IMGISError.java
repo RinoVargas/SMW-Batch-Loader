3dfcb47c762d7a5d44667cbd7af8e9e58d48efc7
Rino Varas Quintana <rino.vargas.quintana@gmail.com>
2021-10-01 16:35:09 -0500

    Remove duplicate word

--- src/main/java/cl/entel/eai/constants/IMGISError.java
+++ src/main/java/cl/entel/eai/constants/IMGISError.java
@@ -5,7 +5,7 @@ public enum IMGISError {
     ERROR_DB_UNAVAILABLE_DISCONNECTION(99, "Fallo al hacer desconexión con la base de datos"),
     ERROR_DB_NOT_CONNECTED(99, "No se pudo conectar a la base de datos"),
     ERROR_DB_UNKNOWN_ERROR(99, "Error al consultar a la base de datos"),
-    ERROR_UNKNOWN_ERROR(99, "Error error desconocido en el sistema");
+    ERROR_UNKNOWN_ERROR(99, "Error desconocido en el sistema");
 
     private int code;
     private String message;
